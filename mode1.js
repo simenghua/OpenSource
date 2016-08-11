37b2c05578c4a9fd953304937ba22a30b5a28715
simenghua <shua@pdx.edu>
2016-08-10 22:22:45 -0700

    mode1 function update with move

--- mode1.js
+++ mode1.js
@@ -193,3 +193,56 @@ function moveUp(){}
 function moveRight(){}
 
 function moveDown(){}
+
+/*--------------------------- canMove --------------------------------*/
+function canMoveLeft(board){
+	for(var i = 0; i < 4; i++){
+		for(var j = 1; j < 4; j++) {
+			if(board[i][j] != 0){
+				if(board[i][j-1] == 0 || board[i][j] == board[i][j-1]){
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+function canMoveUp(board){
+	for(var i = 1; i < 4; i++){
+		for(var j = 0; j < 4; j++) {
+			if(board[i][j] != 0){
+				if(board[i-1][j] == 0 || board[i][j] == board[i-1][j]){
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+function canMoveRight(board){
+	for(var i = 0; i < 4; i++){
+		for(var j = 0; j < 3; j++) {
+			if(board[i][j] != 0){
+				if(board[i][j+1] == 0 || board[i][j] == board[i][j+1]){
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+function canMoveDown(board){
+	for(var i = 0; i < 3; i++){
+		for(var j = 0; j < 4; j++) {
+			if(board[i][j] != 0){
+				if(board[i+1][j] == 0 || board[i][j] == board[i+1][j]){
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
