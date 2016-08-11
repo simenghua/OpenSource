b0305b8115f8f036dc5d444260898a3779d0e2ef
simenghua <shua@pdx.edu>
2016-08-10 21:15:25 -0700

    mode1 function update with initBoard

--- mode1.js
+++ mode1.js
@@ -9,3 +9,33 @@ function newgame() {
 	generateNumb();
 }	
 
+/*------------------ InitiateBoard -----------------*/
+var board = new Array();
+var addAlready = new Array();
+
+function init() {
+	for(var i = 0; i < 4; i++) {
+		for(var j = 0; j < 4; j++) {
+			var gridtile = $("#grid-tile-" + i + "-" + j);
+			gridtile.css('top', getposTop(i, j));
+			gridtile.css('left', getposLeft(i, j));
+			board[i] = new Array();
+			addAlready[i] = new Array();
+		}
+	}
+	for(var i = 0; i < 4; i++){
+		board[i] = new Array();
+		addAlready[i] = new Array();
+		for(var j = 0; j < 4; j++){
+			board[i][j] = 0;
+		}
+	}
+}  
+
+function getposTop(i, j) {
+	return 20 + i * 120;
+}
+
+function getposLeft(i, j) {
+	return 20 + j * 120;
+}
