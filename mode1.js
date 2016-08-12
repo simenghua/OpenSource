1020ce598156ed93cf67d66edbbf251e6cb1b43d
simenghua <shua@pdx.edu>
2016-08-12 16:11:24 -0700

    Update mode1.js

--- mode1.js
+++ mode1.js
@@ -1,3 +1,7 @@
+//Copyright (c) 2016 Simeng Hua
+//This code is available under the "MIT License".
+//Please see the file COPYING in this distribution
+//for license terms.
 
 /*---------------------------- NewGame ------------------------*/
 $(document).ready(function() {
